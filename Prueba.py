fa2b62727331e22806281369b3626bbf4cd05ac7
Jeronimo Alejandre Rosa <jeronimoar3d@gmail.com>
2016-11-24 00:29:40 +0100

    Primer programa
    Este es mi primer programa en python, he tenido errores sobre todo en el
    input, no me he dado cuenta que en la version  3 no es "raw_imput" sino
    imput.

---
+++ Prueba.py
@@ -0,0 +1,11 @@
+# _*_ coding : utf-8 _*_
+
+#Este es mi primer programa en python
+
+print("si mi edad es 18 , ¿cuantos años tendré en 2020?")
+
+if input() == "22":
+    print("Has acertado!")
+else:
+    print("Meh... intentalo otra vez!")
+
